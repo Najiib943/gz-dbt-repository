d036471b5254cc99f3dca551cf2601f898a69430
najiiib2023@gmail.com <najiiib2023@gmail.com>
2024-12-10 16:10:55 +0000

    margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,6 +1,5 @@
 with sales_margin as (
 
-    -- Use the int_sales_margin model as the source
     select * 
     from {{ ref('int_sales_margin') }}
 
@@ -8,13 +7,9 @@ with sales_margin as (
 
 aggregated_orders as (
 
-    -- Aggregate data to calculate order-level metrics
     select
         orders_id,
-        -- Use the earliest date_date for each orders_id
         min(date_date) as order_date,
-        
-        -- Calculate order-level metrics
         sum(revenue) as total_revenue,
         sum(purchase_cost) as total_purchase_cost,
         sum(margin) as total_margin
