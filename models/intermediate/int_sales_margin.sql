6b23b262302680be0a82c3c84e0c12eca17bc7fc
najiiib2023@gmail.com <najiiib2023@gmail.com>
2024-12-10 17:05:52 +0000

    2

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,36 +1,48 @@
-with
-    sales_data as (
-        select orders_id, products_id, revenue, quantity
-        from {{ source("raw", "sales") }}
+WITH sales_data AS (
+    SELECT 
+        orders_id,
+        products_id,
+        revenue,
+        -- Cast quantity to INT64 if it is currently stored as STRING
+        CAST(quantity AS INT64) AS quantity
+    FROM `galvanic-flame-442110-g0.gz_raw_data.raw_gz_sales`
+),
 
-    ),
+product_data AS (
+    SELECT 
+        products_id,
+        -- Cast purchase_price to FLOAT64 if it is stored as STRING
+        CAST(purchase_price AS FLOAT64) AS purchase_price
+    FROM `galvanic-flame-442110-g0.gz_raw_data.raw_gz_product`
+),
 
-    product_data as (
-        select products_id, purchase_price from {{ source("raw", "product") }}
-    ),
+joined_data AS (
+    SELECT
+        s.orders_id,
+        s.products_id,
+        s.revenue,
+        s.quantity,
+        p.purchase_price,
+        -- Calculate purchase cost as quantity * purchase price
+        s.quantity * p.purchase_price AS purchase_cost
+    FROM sales_data s
+    INNER JOIN product_data p
+        ON s.products_id = p.products_id
+),
 
-    joined_data as (
-        select
-            s.orders_id,
-            s.products_id,
-            s.revenue,
-            s.quantity,
-            p.purchase_price,
-            s.quantity * p.purchase_price as purchase_cost
-        from sales_data s
-        inner join product_data p on s.products_id = p.products_id
-    ),
+calculated_data AS (
+    SELECT
+        orders_id,
+        products_id,
+        revenue,
+        quantity,
+        purchase_price,
+        purchase_cost,
+        -- Calculate margin as revenue - purchase cost
+        revenue - purchase_cost AS margin
+    FROM joined_data
+)
 
-    calculated_data as (
-        select
-            orders_id,
-            products_id,
-            revenue,
-            quantity,
-            purchase_price,
-            purchase_cost,
-            revenue - purchase_cost as margin
-
-        from joined_data
-    )
-
+-- Final select query to retrieve the results without LIMIT
+SELECT * 
+FROM calculated_data;
