b1e90b554e5d1513da8d75a8c13bfe85baaa3650
najiiib2023@gmail.com <najiiib2023@gmail.com>
2024-12-10 22:32:06 +0000

    finance

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,44 +1,18 @@
-WITH sales_data AS (
-    SELECT
-        date_date,
-        orders_id,
-        revenue,
-        quantity,
-        p.products_id,
-        p.purchase_price
-    FROM {{ source('raw', 'sales') }} s
-    LEFT JOIN {{ source('raw', 'product') }} p
-        ON s.pdt_id = p.products_id
-),
+ -- finance_days.sql
 
-shipping_data AS (
-   
-    SELECT
-        s.orders_id,
-        s.shipping_fee,
-        s.log_cost,
-        s.ship_cost
-    FROM  {{ source('raw', 'ship') }} s
-),
-
-
-aggregated_data AS (
-    SELECT
-        sd.date_date,
-        COUNT(DISTINCT sd.orders_id) AS total_transactions,
-        SUM(sd.revenue) AS total_revenue,
-        AVG(sd.revenue / NULLIF(sd.quantity, 0)) AS avg_basket,
-        SUM(sd.revenue - (sd.quantity * sd.purchase_price)) AS operational_margin,  
-        SUM(sh.shipping_fee) AS total_shipping_fee,
-        SUM(sh.log_cost) AS total_log_cost,
-        SUM(sd.quantity) AS total_quantity_sold
-    FROM sales_data sd
-    LEFT JOIN shipping_data sh
-        ON sd.orders_id = sh.orders_id
-    GROUP BY sd.date_date
-)
-
-
-SELECT *
-FROM aggregated_data
-ORDER BY date_date;
+ SELECT
+     date_date
+     ,COUNT(orders_id) AS nb_transactions
+     ,ROUND(SUM(revenue),0) AS revenue
+     ,ROUND(AVG(revenue),1) AS average_basket
+     ,ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis
+     ,ROUND(SUM(margin),0) AS margin
+     ,ROUND(SUM(operational_margin),0) AS operational_margin
+     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
+     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
+     ,ROUND(SUM(logcost),0) AS logcost
+     ,ROUND(SUM(ship_cost),0) AS ship_cost
+     ,SUM(quantity) AS quantity
+ FROM {{ref("int_orders_operational")}}
+ GROUP BY  date_date
+ ORDER BY  date_date DESC
