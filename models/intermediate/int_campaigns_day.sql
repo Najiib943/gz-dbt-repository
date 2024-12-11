c570599b0dc8b6805cb325139ddf28b922580fa8
najiiib2023@gmail.com <najiiib2023@gmail.com>
2024-12-11 15:53:46 +0000

    5

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,5 +1,5 @@
 
-  SELECT
+ SELECT
     date_date,
     paid_source,
     campaign_key,
@@ -9,10 +9,11 @@
     SUM(click) AS total_click  -- Sum of clicks
 
  FROM {{ ref("int_campaigns") }}
-GROUP BY
-    date_date,
-    paid_source,
-    campaign_key,
-    campaign_name
- ORDER BY date_date DESC;
+ GROUP BY 
+    date_date,  -- Group by date_date
+    paid_source,  -- Group by paid_source
+    campaign_key,  -- Group by campaign_key
+    campaign_name  -- Group by campaign_name
+ ORDER BY date_date DESC
+ 
  
